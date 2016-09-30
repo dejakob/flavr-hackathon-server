3102306f6eb63e66555e9e347832c004c76b3ce4
Jakob Kerkhove <jakobvti@gmail.com>
2016-09-30 22:32:44 +0200

    Add meal type and data

---
+++ types/meal.type.js
@@ -0,0 +1,41 @@
+/**
+ * @name Chef
+ * @property {GraphQLString} id
+ * @property {GraphQLInt} availableServings
+ * @property {GraphQLInt} maxServings
+ * @property {GraphQLInt} price
+ * @property {Chef} chef
+ * @property {GraphQLString} story
+ * @property {GraphQLString} description
+ * @property {GraphQLString} title
+ * @property {GraphQLString} picture
+ */
+
+// Import GraphQL
+const graphQl = require('graphql/type');
+const {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString
+} = graphQl;
+
+const Chef = require('./chef.type');
+
+// Create a Chef GraphQL Object type
+const MealType = new GraphQLObjectType({
+    name: 'Chef',
+    fields: {
+        id: { type: GraphQLString },
+        availableServings: { type: GraphQLInt },
+        maxServings: { type: GraphQLInt },
+        price: { type: GraphQLInt },
+        chef: { type: Chef },
+        story: { type: GraphQLString },
+        description: { type: GraphQLString },
+        title: { type: GraphQLString },
+        picture: { type: GraphQLString }
+    }
+});
+
+// Export the type
+module.exports = MealType;
